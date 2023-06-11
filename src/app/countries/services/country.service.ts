0b62178d7f5c365cef941e37294df27efe6c71b7
Bladimir Gonzales <bladimirg@gmail.com>
2023-06-11 15:20:36 +0200

    small fixes

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({
@@ -9,26 +11,34 @@ import { Country } from '../interfaces/country.interface';
 export class CountryService {
   private apiUrl = 'https://restcountries.com/v2';
 
+  get HttpParams(){
+    return new HttpParams()
+    .set('fields','name,capital,alpha2Code,flag,population');
+  };
+
+
   public searchCountry(txtSearch:string):Observable<Country[]>{
     const url = `${this.apiUrl}/name/${txtSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params:this.HttpParams});
   }
 
   
   public searchByCapital(txtSearch:string):Observable<Country[]>{
     const url = `${this.apiUrl}/capital/${txtSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params:this.HttpParams});
   }
 
   public getCountryByCode(txtCountryCode:string):Observable<Country>{
     const url = `${this.apiUrl}/alpha/${txtCountryCode}`;
     return this.http.get<Country>(url);
   }
-
   
   public searchByRegion(txtSearch:string):Observable<Country[]>{
     const url = `${this.apiUrl}/region/${txtSearch}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params:this.HttpParams})
+    .pipe(
+      tap(console.log)
+    );
   }
 
   
